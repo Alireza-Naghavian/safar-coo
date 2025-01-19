657a69e46ddf03f0cf45f8a749a4ecb53b173d25
alireza <alirezanghngh1379@gmail.com>
2025-01-19 10:47:33 +0330

    Feat(notifications):Add DropDown for notifications

---
+++ src/components/molecules/DropDowns/NotifDropDown.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { ChildrenProps, ClassName_T } from "@/types/global.t";
+import { DirectNormal, SmsNotification } from "iconsax-react";
+import React from "react";
+type DropDownProps_T = ClassName_T &
+  ChildrenProps & {
+    trigger: (id: string) => void;
+    id: string;
+    isOpen: boolean;
+  };
+function NotifDropDown({ trigger, children, ...rest }: DropDownProps_T) {
+  return (
+    <div
+      id={rest.id}
+      onClick={() => trigger(rest.id)}
+      className={`relative w-full py-6 px-3 flex  rounded-8
+        items-start justify-between bg-accent-400  h-full tr-300 cursor-pointer
+         ${
+           rest.isOpen
+             ? "sm:max-h-[263px] max-h-[350px] overflow-y-auto sm:overflow-y-hidden"
+             : "max-h-[100px] child:line-clamp-1"
+         } 
+         ${rest.className}`}
+    >
+      {/* body */}
+      <p
+        className="relative w-full max-w-[220px] sm:max-w-[680px] bg-transparent
+       text-white text-bodyB3Regular "
+      >
+        {children}
+      </p>
+      {/* seeen & unseen */}
+      <div className="flex items-center ">
+        <DirectNormal
+          variant="Bold"
+          className={`size-6 stroke-white ${rest.isOpen ? "block" : "hidden"}`}
+        />
+        <SmsNotification
+          variant="Bold"
+          className={`size-6 stroke-white ${rest.isOpen ? "hidden" : "block"}`}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default NotifDropDown;
---
+++ src/features/user-panel/components/Notifications/MyNotifs.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState } from "react";
+import HeaderContentPanelLayout from "../HeaderContentPanelLayout";
+import CustomSwitch from "@/components/atoms/Switch/CustomSwitch";
+import NotifDropDown from "@/components/molecules/DropDowns/NotifDropDown";
+import { fakeNotifData } from "@/utils/constants";
+
+function MyNotifs() {
+  const [isDropDownOpen, setIsDropDownOpen] = useState<string | null>(null);
+  const toggleDropDown = (id: string) => {
+    setIsDropDownOpen(isDropDownOpen === id ? null : id);
+  };
+  return (
+    <div className="w-full relative  h-full shadow-lg max-h-[645] overflow-y-auto   flex flex-col  gap-y-0 ">
+      <HeaderContentPanelLayout
+        title="اعلان های من "
+        desc="اعلان های شما تا ۳۰ روز نگه داری و بعد از آن خودکار 
+        حدف خواهد شد."
+      >
+        <div className="h-full w-full my-auto flex items-center gap-x-6 md:flex-nowrap flex-wrap md:gap-y-0 gap-y-4">
+          <CustomSwitch
+            className="text-natural-black sm:!text-bodyB3semi !text-bodyB4semi"
+            name="seenNotif"
+            title="فقط خوانده نشده ها"
+            hasTitle
+          />
+          <CustomSwitch
+            className="text-natural-black sm:!text-bodyB3semi !text-bodyB4semi"
+            name="unSeenNotif"
+            title="فقط خوانده شده ها"
+            hasTitle
+          />
+        </div>
+      </HeaderContentPanelLayout>
+      <div className="flex flex-col  sm:px-11 px-4 py-8   space-y-8 ">
+        {fakeNotifData.map((notif) => {
+          return (
+            <NotifDropDown
+              key={notif.id}
+              id={notif.id}
+              isOpen={isDropDownOpen === notif.id }
+              trigger={toggleDropDown}
+            >
+              <>{notif.content}</>
+            </NotifDropDown>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default MyNotifs;
