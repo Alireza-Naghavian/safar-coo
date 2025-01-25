e57fb320da0a9ac03ecec28dd782fba3efad0644
alireza <alirezanghngh1379@gmail.com>
2025-01-25 12:03:36 +0330

    Fix:(User-panel-menu):Fix burger menu layout

--- src/components/molecules/logoutBtn/LogoutBtn.tsx
+++ src/components/molecules/logoutBtn/LogoutBtn.tsx
@@ -3,7 +3,7 @@ import { LogoutCurve } from 'iconsax-react'
 function LogoutBtn({isMenuOpen}:{isMenuOpen?:boolean}) {
   return (
     <button className={`${isMenuOpen ? "w-full" : "w-auto"} flex gap-x-2 rounded-4 tr-300 items-center py-2 px-3    hover:!bg-accent-200`} >
-        <LogoutCurve  className='stroke-natural-black group-hover:stroke-accent-500 size-8' />
+        <LogoutCurve  className='stroke-natural-black group-hover:stroke-accent-500 size-7' />
        {isMenuOpen &&  <span className='text-bodyB2Regular group text-natural-gray2 group-hover:text-natural-black  '>خروج</span>}
     </button>
   )
--- src/features/user-panel/components/UserPanelMenu.tsx
+++ src/features/user-panel/components/UserPanelMenu.tsx
@@ -13,18 +13,18 @@ type MenuProps = {
   closeMenu: () => void;
 };
 const menuMotionVars = {
-  open: { x: 100, display: "none" },
+  open: { x: 100, display: "none"},
   close: { x: 0, display: "block" },
 };
 function UserPanelMenu({ isMenuOpen, openMenu, closeMenu }: MenuProps) {
   return (
-    <div className="relative w-full rounded-8 bg-gray-100/65 flex flex-col py-4 px-2
-      shadow-lg overflow-hidden">
-      <div className="hidden lg:flex w-fit relative   h-9 ">
-        <AnimatePresence initial={false} mode="sync">
+    <div className={`relative w-full rounded-8 bg-gray-100/65 flex flex-col  py-4 px-2
+      shadow-lg overflow-hidden  `}>
+      <div className={`hidden lg:flex  relative  justify-center h-9  ${isMenuOpen? "w-fit":"w-full"}`}>
+        <AnimatePresence  initial={false} mode="sync">
           <motion.button
             key={"closeUserPanelMenu"}
-            className="cursor-pointer absolute z-50"
+            className="cursor-pointer  w-fit z-50 "
             transition={{duration:.2}}
             variants={menuMotionVars}
             animate={isMenuOpen ? "close" : "open"}
@@ -35,12 +35,12 @@ function UserPanelMenu({ isMenuOpen, openMenu, closeMenu }: MenuProps) {
           <motion.button
            transition={{duration:.2}}
             key={"openUserPanelMenu"}
-            className="cursor-pointer absolute z-50  right-2"
+            className="cursor-pointer mx-auto   w-full  "
             variants={menuMotionVars}
             animate={isMenuOpen ? "open" : "close"}
             onClick={openMenu}
           >
-            <HambergerMenu className=" size-8 stroke-natural-black" />
+            <HambergerMenu className=" size-8 stroke-natural-black mx-auto" />
           </motion.button>
         </AnimatePresence>
       </div>
@@ -49,11 +49,11 @@ function UserPanelMenu({ isMenuOpen, openMenu, closeMenu }: MenuProps) {
       <div className={` flex-col w-full relative tr-200 delay-100 
          gap-y-2 items-center flex`}>
         <Image
-          sizes="68"
+          sizes="60"
           src={"/images/user_sample.png"}
           alt="user.png"
-          width={68}
-          height={68}
+          width={60}
+          height={60}
           className="rounded-full border border-natural-black mt-4"
         />
         <span className={`text-bodyB4Semi text-natural-black ${!isMenuOpen ? "hidden" : "flex"}`}>
@@ -61,7 +61,7 @@ function UserPanelMenu({ isMenuOpen, openMenu, closeMenu }: MenuProps) {
         </span>
       </div>
       {/* user menu content */}
-      <ul  className="w-full relative flex  flex-col gap-y-4 mt-6 ">
+      <ul  className="w-full relative flex   flex-col  gap-y-4 mt-6 ">
         {userMenuItem.map((menuItem) => {
           return (
             <React.Fragment key={menuItem._id}>
@@ -71,9 +71,9 @@ function UserPanelMenu({ isMenuOpen, openMenu, closeMenu }: MenuProps) {
                   {...menuItem}
                   title={isMenuOpen ? menuItem.title :null}
                   Icon={menuItem.Icon}
-                  IconStyles="group-hover:stroke-accent-500 flex  "
+                  IconStyles="group-hover:stroke-accent-500 flex "
                   className="text-bodyB2Regular group text-natural-gray2 tr-200 text-nowrap
-                   hover:text-natural-black  hover:!bg-accent-200 "
+                   hover:text-natural-black  hover:!bg-accent-200   "
                 />
               </li>
             </React.Fragment>
