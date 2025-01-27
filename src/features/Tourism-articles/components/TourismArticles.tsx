2a9b37d71a9396f7063e6744153a5eb1c51a19b0
alireza <alirezanghngh1379@gmail.com>
2025-01-27 18:19:41 +0330

    Feat(toursim-articles):Add aricle card -Responsive page

--- src/features/Tourism-articles/components/TourismArticles.tsx
+++ src/features/Tourism-articles/components/TourismArticles.tsx
@@ -2,12 +2,17 @@ import BreadCrumb from "@/components/atoms/Breadcrumb/BreadCrumb";
 import ClientLayout from "@/components/layouts/ClientLayout";
 import React from "react";
 import ArticlesHeader from "./ArticlesHeader";
+import SectionTitle from "@/components/atoms/SectionTitle/SectionTitle";
+import FullBlogCard from "@/components/molecules/cards/FullBlogCard";
 
 function TourismArticles() {
   return (
     <ClientLayout>
       <main className="max-w-[1920px] relative mx-auto overflow-x-hidden min-h-screen  ">
-        <section className="md:px-[113px] px-5">
+        <div className="md:px-[113px] px-5">
+
+       
+        <section className="">
           {/* filter & filter */}
           <div className="mt-10 w-full">
             <BreadCrumb
@@ -22,6 +27,44 @@ function TourismArticles() {
           </div>
           <ArticlesHeader/>
         </section>
+        <section className="md:mt-40 mt-10 relative w-full">
+            <SectionTitle title="جدیدترین مقالات" className="tracking-tighter" />
+              {/* latest blogs */}
+              <div className="mt-10 w-full flex flex-col gap-y-10  ">
+              <FullBlogCard
+                cover="/images/sample-card.png"
+                date={new Date}
+                desc={`شیراز مهد فرهنگ، ادب و هنر فارسی، با هوایی آکنده از بوی عطر
+               بهار نارنج در فصل بهار، هر مسافری را برای سفر به این شهر وسوسه می‌کند و...`}
+               href=""
+               title="برای سفر به شیراز کجاها بریم؟"
+               variant="full-detail"
+               badgeContent="گردشگری"
+              />
+              <FullBlogCard
+                cover="/images/sample-card.png"
+                date={new Date}
+                desc={`شیراز مهد فرهنگ، ادب و هنر فارسی، با هوایی آکنده از بوی عطر
+               بهار نارنج در فصل بهار، هر مسافری را برای سفر به این شهر وسوسه می‌کند و...`}
+               href=""
+               title="برای سفر به شیراز کجاها بریم؟"
+               variant="full-detail"
+               badgeContent="گردشگری"
+              />
+              <FullBlogCard
+                cover="/images/sample-card.png"
+                date={new Date}
+                desc={`شیراز مهد فرهنگ، ادب و هنر فارسی، با هوایی آکنده از بوی عطر
+               بهار نارنج در فصل بهار، هر مسافری را برای سفر به این شهر وسوسه می‌کند و...`}
+               href=""
+               title="برای سفر به شیراز کجاها بریم؟"
+               variant="full-detail"
+               badgeContent="گردشگری"
+              />
+              </div>
+              {/* inifinite scroll place */}
+        </section>
+        </div>
       </main>
     </ClientLayout>
   );
