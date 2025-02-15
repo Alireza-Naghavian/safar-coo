81c62d30a125a6586701dfa71599dbe105e811cb
alireza <alirezanghngh1379@gmail.com>
2025-02-15 14:38:38 +0330

    Feat(tr-exp):place experience design -Add short Intro & header

---
+++ src/app/experiences/[cityName]/[placeData]/page.tsx
@@ -0,0 +1,10 @@
+import PlaceExpriences from '@/features/experiences/components/PlaceExpriences/PlaceExpriences'
+import React from 'react'
+
+function page() {
+  return (
+    <PlaceExpriences/>
+  )
+}
+
+export default page
---
+++ src/features/experiences/components/PlaceExpriences/PlaceExpriences.tsx
@@ -0,0 +1,95 @@
+import Avatar from "@/components/atoms/Avatar/Avatar";
+import BreadCrumb from "@/components/atoms/Breadcrumb/BreadCrumb";
+import MainBtn from "@/components/atoms/buttons&links/MainBtn";
+import SectionTitle from "@/components/atoms/SectionTitle/SectionTitle";
+import ClientLayout from "@/components/layouts/ClientLayout";
+import TransitionRoute from "@/components/organisms/TransitionRoute/TransitionRoute";
+import { Save2, Share } from "iconsax-react";
+import { BtnBox_T } from "../../experiences.t";
+import ShortIntro from "./ShortIntro";
+
+function PlaceExpriences() {
+  return (
+    <ClientLayout>
+      <TransitionRoute>
+        <main
+          className="max-w-[1920px] relative mx-auto 
+        overflow-x-hidden min-h-screen  "
+        >
+          <div className="md:px-[113px] relative px-5">
+            <div className="mt-10 w-full">
+              <BreadCrumb
+                firstTitle={"صفحه اصلی"}
+                firstTarget="/"
+                nestedStep={3}
+                nestedLinks={[
+                  { title: "تجربه های سفر", target: "/experiences" },
+                  { title: "اصفهان", target: "/esfehan" },
+                  { title: "کلیسای وانگ", target: "/whangChurch" },
+                ]}
+                seperator={">"}
+              />
+            </div>
+            {/* header */}
+            <section className="w-full relative flex flex-col gap-y-8 mt-22 pb-6 border-b border-b-natural-gray2">
+              <div className="w-fit flex items-center gap-x-3">
+                <Avatar />
+                <span className="text-right text-bodyB3Regular text-natural-black">
+                  تجربه گر :سعید
+                </span>
+              </div>
+              <div className="w-full relative flex justify-between">
+                <SectionTitle title="تجربه بازدید از کلیسای وانک اصفهان" />
+                <div className="w-fit flex sm:justify-end justify-start  gap-x-4 gap-y-2">
+                  <BtnBox
+                    title="اشتراک گذاری"
+                    Iconstyeles="fill-accent-700 group-hover:fill-white"
+                    Icon={Share}
+                    className="hover:bg-accent-500 hover:!border-accent-500"
+                  />
+                  <BtnBox
+                    title="اشتراک گذاری"
+                    Iconstyeles="fill-success-1 group-hover:fill-white"
+                    Icon={Save2}
+                    className="hover:bg-success-1 hover:!border-success-1"
+                  />
+                </div>
+              </div>
+            </section>
+            <ShortIntro
+              activeTime={new Date()}
+              address="استان اصفهان، اصفهان، محله جلفا،
+             خیابان نظر شرقی، کوچه کلیسای وانک"
+             coordinates={{lat:"3923423",lng:"9234283942"}}
+             cover="/images/mosque.png"
+             visitCost="FREE"
+             score={4.3}
+             title="کلیسا وانگ"
+            />
+          </div>
+        </main>
+      </TransitionRoute>
+    </ClientLayout>
+  );
+}
+
+const BtnBox = ({ Icon, title, className, Iconstyeles }: BtnBox_T) => {
+  return (
+    <MainBtn
+      size="lg"
+      state="normal"
+      variant="outline"
+      className={`
+            flex items-center rounded-8 gap-2.5 group tr-300
+            text-nowrap
+           ${className}
+            `}
+    >
+      <Icon variant="Bold" className={`size-6  ${Iconstyeles}`} />
+      <span className="group-hover:text-white sm:text-btnTextXl text-btnTextM">
+        {title}
+      </span>
+    </MainBtn>
+  );
+};
+export default PlaceExpriences;
