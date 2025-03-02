6176bb457d414a66b7ee8288e6562d301bb4cfdf
alireza <alirezanghngh1379@gmail.com>
2025-03-02 19:21:25 +0330

    Feat:(auth):add protected route with middleware

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const protectedRoutes = [
+  "/user-panel",
+  "/user-panel/blog-analytics",
+  "/user-panel/my-bookmarks",
+  "/user-panel/notifications",
+  "/user-panel/tickets",
+  "/user-panel/travel-exprience",
+  "/user-panel/user-profile",
+];
+
+export const middleware = async (req: NextRequest) => {
+  const accessToken = req.cookies.get("accessToken")?.value ?? "";
+  const refreshToken = req.cookies.get("refreshToken")?.value ?? "";
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/auth/getMe`, {
+    headers: {
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    },
+  });
+  const data = await res.json();
+  // redirect when not login
+  if (
+    data.status === 401 &&
+    protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))
+  ) {
+    const absoluteUrl = new URL("/", req.nextUrl.origin);
+    return NextResponse.redirect(absoluteUrl.toString());
+  }
+};
+export const config = {
+  matcher: ["/user-panel/:path*"],
+};
