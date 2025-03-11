34565689438fd1dc60009f2d8c6f3228488782fc
alireza <alirezanghngh1379@gmail.com>
2025-03-11 17:21:20 +0330

    Feat(usre-panel):set query strings and retrieve fresh data

--- src/features/user-panel/services/userServices.ts
+++ src/features/user-panel/services/userServices.ts
@@ -1,26 +1,42 @@
 import app from "@/services/services";
-import { EditProfileReq_T, NotificationsType, Ticket_T, TicketBodyReq_T } from "../user-panel.t";
+import {
+  EditProfileReq_T,
+  NotificationsType,
+  Ticket_T,
+  TicketBodyReq_T,
+} from "../user-panel.t";
 
-export const createTicketReq = async(data:TicketBodyReq_T)=>{
-return app.post("/user/tickets",data).then(({data})=>data)
-}
-export const getTicketsReq = async():Promise<Ticket_T[]>=>{
-    return app.get("/user/tickets").then(({data})=>data)
-}
-export const getTicketReq = async({id}:{id:string}):Promise<Ticket_T>=>{
-    return app.get(`/user/ticket/${id}`).then(({data})=>data)
-}
+export const createTicketReq = async (data: TicketBodyReq_T) => {
+  return app.post("/user/tickets", data).then(({ data }) => data);
+};
+export const getTicketsReq = async (): Promise<Ticket_T[]> => {
+  return app.get("/user/tickets").then(({ data }) => data);
+};
+export const getTicketReq = async ({
+  id,
+}: {
+  id: string;
+}): Promise<Ticket_T> => {
+  return app.get(`/user/ticket/${id}`).then(({ data }) => data);
+};
 
-export const getTicketByQueryReq = async(query:string):Promise<Ticket_T[]>=>{
-return app.get(`/user/tickets?status=${query}`).then(({data})=>data)
-}
-export const updateUserInfoReq  = async(data:EditProfileReq_T)=>{
-return app.patch("/auth/user/edit",data).then(({data})=>data)
-}
+export const getTicketByQueryReq = async (
+  query: string
+): Promise<Ticket_T[]> => {
+  return app.get(`/user/tickets?status=${query}`).then(({ data }) => data);
+};
+export const updateUserInfoReq = async (data: EditProfileReq_T) => {
+  return app.patch("/auth/user/edit", data).then(({ data }) => data);
+};
 
-export const getNotificationsReq = async():Promise<NotificationsType[]>=>{
-    return app.get(`/notif/all`).then(({data})=>data)
-    }
-export const MarkAsReadReq = async({notifId}:{notifId:string})=>{
-    return app.patch(`/notif/update`,{notifId}).then(({data})=>data)
-    }
+export const getNotificationsReq = async (): Promise<NotificationsType[]> => {
+  return app.get(`/notif/all`).then(({ data }) => data);
+};
+export const getNotificationsByQueryReq = async (
+  query: string
+): Promise<NotificationsType[]> => {
+  return app.get(`/notif/all?status=${query}`).then(({ data }) => data);
+};
+export const MarkAsReadReq = async ({ notifId }: { notifId: string }) => {
+  return app.patch(`/notif/update`, { notifId }).then(({ data }) => data);
+};
